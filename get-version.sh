985226318e18f9e44a96aee53f4541b0bb23bfe0
Le Stephane <contact@stephane.today>
2022-08-17 12:02:17 +0300

    Added support for multi-modules + maven wrapper (mvnw) #2

--- get-version.sh
+++ get-version.sh
@@ -1,1 +1,9 @@
-cd $POMPATH && mvn help:evaluate -Dexpression=project.version -q -DforceStdout
+#!/usr/bin/env sh
+
+if [ -x "$POMPATH/mvnw" ]; then
+    MAVEN="$POMPATH/mvnw"
+else
+    MAVEN="mvn"
+fi
+
+"${MAVEN}" -f "$POMPATH" help:evaluate -Dexpression=project.version -q -DforceStdout
